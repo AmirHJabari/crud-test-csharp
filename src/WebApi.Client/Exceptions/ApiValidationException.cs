bfbc4b5858503ef18985f3220f7029ca3afd5aad
Amir H. Jabari <amirhamzehjabari@outlook.com>
2023-01-09 03:31:30 +0330

    move WebApi and WebUI to src folder Initialize WebApi.Client

---
+++ src/WebApi.Client/Exceptions/ApiValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace WebApi.Client.Exceptions;
+
+public class ApiValidationException : Exception
+{
+    public ApiValidationException()
+        : base("One or more validation failures have occurred.")
+    {
+        Result = new ApiResult<IDictionary<string, string[]>>(false, Message, new Dictionary<string, string[]>());
+    }
+
+    public ApiValidationException(ApiResult<IDictionary<string, string[]>> apiResult)
+        : base("One or more validation failures have occurred.")
+    {
+        Result = apiResult;
+    }
+
+    public ApiResult<IDictionary<string, string[]>> Result { get;private set; }
+}
---
+++ src/WebApi.Client/HttpClients/CustomerHttpClient.cs
@@ -0,0 +1,76 @@
+using System.Net.Http.Json;
+using System.Net;
+using Application.Entities.Customers.Commands.CreateCustomerCommand;
+using System.Text.Json;
+using Application.Entities.Customers.Queries.GetCustomerById;
+
+namespace WebApi.Client.HttpClients;
+
+public class CustomerHttpClient : IDisposable
+{
+    private readonly HttpClient _client;
+
+    public CustomerHttpClient(string apiBaseAddress)
+    {
+        _client = new HttpClient()
+        {
+            BaseAddress = new Uri(apiBaseAddress)
+        };
+    }
+
+    /// <exception cref="ApiValidationException"/>
+    /// <exception cref="ApiBaseException"/>
+    public async Task<ApiResult<int>> CreateAsync(CreateCustomerCommand request, CancellationToken cancellationToken = default)
+    {
+        var res = await _client.PostAsJsonAsync("api/v1/Customers", request, cancellationToken);
+
+        if (!res.IsSuccessStatusCode)
+        {
+            throw await GetPropperException(res);
+        }
+
+        return await res.Content.ReadFromJsonAsync<ApiResult<int>>();
+    }
+
+    /// <exception cref="ApiValidationException"/>
+    /// <exception cref="ApiNotFoundException"/>
+    /// <exception cref="ApiBaseException"/>
+    public async Task<ApiResult<CustomerDto>> GetCustomerByIdAsync(GetCustomerById request, CancellationToken cancellationToken = default)
+    {
+        var res = await _client.GetAsync($"api/v1/Customers?Id={request.Id}", cancellationToken);
+
+        if (!res.IsSuccessStatusCode)
+        {
+            throw await GetPropperException(res);
+        }
+
+        return await res.Content.ReadFromJsonAsync<ApiResult<CustomerDto>>();
+    }
+
+    private async Task<Exception> GetPropperException(HttpResponseMessage res)
+    {
+        switch (res.StatusCode)
+        {
+            case HttpStatusCode.BadRequest:
+                {
+                    var apiResult = await res.Content.ReadFromJsonAsync<ApiResult<IDictionary<string, string[]>>>();
+                    return new ApiValidationException(apiResult);
+                }
+            case HttpStatusCode.NotFound:
+                {
+                    var apiResult = await res.Content.ReadFromJsonAsync<ApiResult>();
+                    return new ApiNotFoundException(apiResult.Message, apiResult);
+                }
+            default:
+                {
+                    var apiResult = await res.Content.ReadFromJsonAsync<ApiResult>();
+                    return new ApiBaseException(apiResult.Message, apiResult);
+                }
+        }
+    }
+
+    public void Dispose()
+    {
+        _client.Dispose();
+    }
+}
